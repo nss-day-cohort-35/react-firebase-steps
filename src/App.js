9e64d248afb19e558ee0ca6e65e547704f50abb9
Brenda Long <brenda@studio6thave.com>
2020-01-16 10:22:21 -0600

    started

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
+import firebase from 'firebase/app';
+import 'firebase.auth';
+import fbConnection from './helpers/data/fbConnection';
 import './App.scss';
+
+//make a connection when app first loads
+fbConnection();
+
 
 class App extends Component {
   state = {
 
   }
 
+  authListener = () => firebase.auth().onAuthStateChanged((user) => {
+    console.log("authListener called", user);
+  })
+
   componentDidMount() {
     // console.log("componentDidMount called");
+    this.authListener();
   }
 
   componentWillUnmount() {
